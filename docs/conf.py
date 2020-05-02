be4f06b23a10eff93eede0595a71a2fa092281e4
Jan Krepl <jankrepl@yahoo.com>
2020-05-02 17:53:37 +0200

    Add additional path in config

--- docs/conf.py
+++ docs/conf.py
@@ -11,8 +11,13 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
+import pathlib
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+
+some_path = pathlib.Path(os.path.abspath('.'))
+parent_some_path = some_path.parent
+sys.path.insert(0, str(some_path))
+sys.path.insert(0, str(parent_some_path))
 
 # -- Project information -----------------------------------------------------
 
